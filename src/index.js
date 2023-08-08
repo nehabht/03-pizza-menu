4cb3aed6063c8afa1a2d8cc2472b4e97a2d6c8a2
Neha <ne.bhatti2@icloud.com>
2023-08-08 20:18:52 +0200

    extracting jsx into new components

--- src/index.js
+++ src/index.js
@@ -129,18 +129,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We are open until {closeHour}:00. Come visti us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We are happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We are open until {props.closeHour}:00. Come visti us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
